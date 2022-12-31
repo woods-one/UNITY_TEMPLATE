0e5c60c8dfcd1f6b8fad98b6d8cf445db49f7cd4
woods-one <0820ricer@gmail.com>
2022-12-31 23:30:32 +0900

    first commit

---
+++ Assets/MyAssets/Scripts/Sample.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+   void Simple ()
+   {
+      Debug.Log("サンプルですよ");
+   }
+}
